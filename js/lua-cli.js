6f165da67fb890e3bd9e21ecb2345c5e7ca5655f
Mike Mihailov <photo.mickey@gmail.com>
2022-07-27 20:41:30 +0300

    Add lua cli

---
+++ js/lua-cli.js
@@ -0,0 +1,15 @@
+let luacli = new LuaCli( "lua-cli" );
+
+function LuaCli ( id ) {
+  let cli = document.getElementById( id );
+  this.add = function ( text ) {
+    let p = document.createElement( 'p' );
+    p.innerText = '>>' + text;
+    cli.appendChild( p );
+    return;
+  }
+  this.clean = function () {
+    cli.innerHTML = '';
+    return;
+  }
+}
