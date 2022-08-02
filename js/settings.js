c2298c8ba6294302fe5dbb795d555cc06bfa62ef
Mike Mihailov <photo.mickey@gmail.com>
2022-08-02 10:21:28 +0300

    settings

--- js/settings.js
+++ js/settings.js
@@ -10,72 +10,98 @@ function Settings () {
   this.data      = null;
   this.init = async function () {
     return new Promise( async function ( resolve ) {
+      initDomList();
       fs.readFile( pathFile, 'utf-8', async function ( error, data ) {
         if ( error == null ) {
           self.data  = JSON.parse( data );
           this.ready = true;
-          await writeToDom();
+          writeToDom();
+          resolve();
+        } else {
           readFromDom();
+          saveSettings();
           resolve();
         }
       });
     });
   }
-  function readFromDom () {
-    for ( var i=0; i<domList.length; i++ ) {
-      let classList = domList[i].classList.value;
-      let path      = domList[i].split( '_' );
-      if ( classList.indexOf( 'form-control' ) > -1 ) {
-        console.log( path );
-      } else if ( classList.indexOf( 'check-input' ) > -1 ) {
 
-      }
-    }
+  function initDomList () {
+    domList = [
+      document.getElementById( 'toolchainPath' ),
+      document.getElementById( 'libPath' ),
+      document.getElementById( 'luacheck_enb' ),
+      document.getElementById( 'lualink_enb' ),
+      document.getElementById( 'luamin_enb' ),
+      document.getElementById( 'luamake_enb' ),
+      document.getElementById( 'loading_auto' ),
+      document.getElementById( 'luamin_spaces_enb' ),
+      document.getElementById( 'luamin_endString_enb' ),
+      document.getElementById( 'luamin_names_enb' )
+    ];
+    domList.forEach( function ( obj ) {
+      obj.addEventListener( 'change', function () {
+        readFromDom();
+        saveSettings();
+      });
+    });
     return;
   }
-  async function writeToDom () {
+
+  async function saveSettings () {
     return new Promise( async function ( resolve ) {
-      domList = [];
-      let keys = Object.keys( self.data );
-      for ( var i=0; i<keys.length; i++ ) {
-        let key = keys[i];
-        if ( typeof(self.data[keys[i]]) == 'string' ) {
-          let output = document.getElementById( keys[i] );
-          if ( output != null ) {
-            domList.push( output );
-            output.value = self.data[keys[i]];
-          }
-        } else {
-          let subkeys = Object.keys( self.data[keys[i]] );
-          for (var j=0; j<subkeys.length; j++ ) {
-            let value = self.data[keys[i]][subkeys[j]];
-            if ( typeof( value ) == 'boolean' ) {
-              let output = document.getElementById( key + '_' + subkeys[j] );
-              if ( output != null ) {
-                domList.push( output );
-                output.checked = value; 
-             }
-            } else if ( typeof( value ) == 'string' ) {
-            
-            } else if ( typeof( value ) == 'object' ) {
-              for ( var s=0; s<value.length; s++ ) {
-                optionKeys = Object.keys( value[s] )
-                for ( var f=0; f<optionKeys.length; f++ ) {
-                  if ( optionKeys[f] == 'enb' ) {
-                    let output = document.getElementById( key + '_' + value[s].id + '_enb' );
-                    if ( output != null ) {
-                      domList.push( output );
-                      output.checked = value[s].enb;
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+      let data = JSON.stringify( self.data );
+      await fs.writeFileSync( pathFile, data );
       resolve();
     });
+  }
+  
+  function readFromDom () {
+    self.data.toolchainPath = document.getElementById( 'toolchainPath' ).value;
+    self.data.libPath       = document.getElementById( 'libPath' ).value;
+    self.data.luacheck.enb  = document.getElementById( 'luacheck_enb' ).checked;
+    self.data.lualink.enb   = document.getElementById( 'lualink_enb' ).checked;
+    self.data.luamin.enb    = document.getElementById( 'luamin_enb' ).checked;
+    self.data.luamake.enb   = document.getElementById( 'luamake_enb' ).checked;
+    self.data.loading.enb   = document.getElementById( 'loading_auto' ).checked;
+    self.data.luamin.keys.forEach( function ( key ) {
+      switch ( key.id ) {
+        case "spaces":
+          key.enb = document.getElementById( 'luamin_spaces_enb' ).checked;
+          break;
+        case "endString":
+          key.enb = document.getElementById( 'luamin_endString_enb' ).checked;
+          break;
+        case "names":
+          key.enb = document.getElementById( 'luamin_names_enb' ).checked;
+          break;    
+      }
+    });
+    return;
+  }
+
+  async function writeToDom () {
+    document.getElementById( 'toolchainPath' ).value  = self.data.toolchainPath;
+    document.getElementById( 'libPath' ).value        = self.data.libPath;
+    document.getElementById( 'luacheck_enb' ).checked = self.data.luacheck.enb;
+    document.getElementById( 'lualink_enb' ).checked  = self.data.lualink.enb;
+    document.getElementById( 'luamin_enb' ).checked   = self.data.luamin.enb;
+    document.getElementById( 'luamake_enb' ).checked  = self.data.luamake.enb;
+    document.getElementById( 'loading_auto' ).checked = self.data.loading.enb;
+    self.data.luamin.keys.forEach( function ( key ) {
+      switch ( key.id ) {
+        case "spaces":
+          document.getElementById( 'luamin_spaces_enb' ).checked = key.enb;
+          break;
+        case "endString":
+          document.getElementById( 'luamin_endString_enb' ).checked = key.enb;
+          break;
+        case "names":
+          document.getElementById( 'luamin_names_enb' ).checked = key.enb;
+          break;    
+      }
+    });
+    return;
   }
   return;
 }
